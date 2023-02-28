f605d1c249ffbe51ecf495374f3116cad4a13420
Vaipik <n.khylko@gmail.com>
2023-02-28 18:54:20 +0200

    user_auth sign_in form

--- src/apps/user_auth/forms.py
+++ src/apps/user_auth/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .libs import constants
 
@@ -10,6 +10,7 @@ User = get_user_model()
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(
+        min_length=constants.USER_MIN_LEGNTH,
         max_length=constants.USER_MAX_LEGNTH,
         label="Enter username",
         widget=forms.TextInput(attrs={
@@ -38,3 +39,24 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username"]
+
+
+class SignInForm(AuthenticationForm):
+    username = forms.CharField(
+        min_length=constants.USER_MIN_LEGNTH,
+        max_length=constants.USER_MAX_LEGNTH,
+        label="Enter username",
+        widget=forms.TextInput(attrs={
+            "class": "form-control mt-3",
+            "placeholder": "username",  # required to prettify view of modal form
+        })
+    )
+    password = forms.CharField(
+        min_length=constants.PASSWORD_MIN_LENGTH,
+        max_length=constants.PASSWORD_MAX_LENGTH,
+        label="Enter password",
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control mt-3",
+            "placeholder": "password",  # required to prettify view of modal form
+        })
+    )
