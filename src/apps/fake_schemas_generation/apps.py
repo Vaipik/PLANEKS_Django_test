d36a4ed3186d7aa9ec07bae7c98cc87212d09134
Vaipik <n.khylko@gmail.com>
2023-02-28 16:35:28 +0200

    apps registered

--- src/apps/fake_schemas_generation/apps.py
+++ src/apps/fake_schemas_generation/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FakeSchemasGenerationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fake_schemas_generation'
+    name = 'apps.fake_schemas_generation'
