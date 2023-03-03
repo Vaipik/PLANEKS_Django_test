deba29bc18d69802be671e98ddc23194e7f190b8
Vaipik <n.khylko@gmail.com>
2023-03-03 14:28:44 +0200

    exception handlers

--- src/config/settings.py
+++ src/config/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') or 123
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "storages",
     'apps.fake_schemas_generation',
     'apps.user_auth',
+    "apps.exceptions",
 ]
 
 MIDDLEWARE = [
--- src/config/urls.py
+++ src/config/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path("", include("apps.user_auth.urls", namespace="user_auth")),
     path("", include("apps.fake_schemas_generation.urls", namespace="fake_schemas_generation"))
 ]
+
+handler400 = "exceptions.views.handler_400"
+handler403 = "exceptions.views.handler_403"
+handler404 = "exceptions.views.handler_404"
+handler500 = "exceptions.views.handler_500"
